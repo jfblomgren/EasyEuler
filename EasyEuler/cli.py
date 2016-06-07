3351289006e201748b6fc27b43beef2457500f11
Encrylize <encrylize@gmail.com>
2016-06-07 16:57:59 +0200

    Move path validation

--- EasyEuler/cli.py
+++ EasyEuler/cli.py
@@ -33,19 +33,26 @@ def generate(problem, language, path, overwrite):
 @click.argument('path', type=click.Path(exists=True, readable=True), nargs=-1)
 def verify(path, language):
     for path_ in path:
-        if os.path.isdir(path_):
-            click.echo('Skipping %s because it is a directory' %
-                       click.format_filename(path_))
+        valid, status, output = process_path(path_, language)
+        if not valid:
             continue
-
-        problem_id = get_problem_id(path_)
-        if problem_id is None or get_problem(problem_id) is None:
-            click.echo('Skipping %s because because it does not contain ' \
-                       'a valid problem ID' % click.format_filename(path_))
-            continue
-
-        status, output = verify_solution(path_, problem_id, language)
 
         click.echo('Checking output of %s: %s' % (click.format_filename(path_),
                                                   output))
         click.echo({'C': 'Correct', 'I': 'Incorrect', 'E': 'Error'}[status])
+
+
+def process_path(path, language):
+    if os.path.isdir(path):
+        click.echo('Skipping %s because it is a directory' %
+                   click.format_filename(path))
+        return False, None, None
+
+    problem_id = get_problem_id(path)
+    if problem_id is None or get_problem(problem_id) is None:
+        click.echo('Skipping %s because it does not contain ' \
+                   'a valid problem ID' % click.format_filename(path))
+        return False, None, None
+
+    status, output = verify_solution(path, problem_id, language)
+    return True, status, output
