1535ba6ba6fa108077e4a55c12097ab62faefae5
Encrylize <encrylize@gmail.com>
2016-07-12 20:40:12 +0200

    Add test coverage reports

--- test.py
+++ test.py
@@ -1,5 +1,20 @@
 import unittest
+import coverage
+import os
 
 
 tests = unittest.TestLoader().discover('tests')
+cov = coverage.coverage(branch=True, include='EasyEuler/*')
+BASE_PATH = os.path.abspath(os.path.dirname(__file__))
+COVERAGE_PATH = os.path.join(BASE_PATH, 'tmp/coverage')
+
+cov.start()
 unittest.TextTestRunner(verbosity=2).run(tests)
+cov.stop()
+cov.save()
+
+print('Coverage Summary:')
+cov.report()
+cov.html_report(directory=COVERAGE_PATH)
+print('HTML version: %s/index.html' % COVERAGE_PATH)
+cov.erase()
