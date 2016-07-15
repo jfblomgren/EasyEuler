b53e53e6098af55254977b9b98af277de8304fbe
Encrylize <encrylize@gmail.com>
2016-07-15 16:15:30 +0200

    Add colorama to requirements for Windows

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
+import sys
+
 from setuptools import setup
+
+
+requirements = ['Click', 'Jinja2']
+
+if 'win32' in sys.platform.lower():
+    # Windows needs colorama for the terminal colors to work.
+    requirements.append('colorama')
 
 setup(
     name='EasyEuler',
     version='0.1',
     py_modules=['EasyEuler'],
-    install_requires=[
-        'Click',
-        'Jinja2'
-    ],
+    install_requires=requirements,
     entry_points='''
         [console_scripts]
         easyeuler=EasyEuler.cli:cli
