9ba552c16c4d55c3ee1c7ec5f4d1e3cbd8cc8437
Encrylize <encrylize@gmail.com>
2016-07-17 13:41:27 +0200

    Add long description

--- setup.py
+++ setup.py
@@ -9,11 +9,17 @@ if 'win32' in sys.platform.lower():
     # Windows needs colorama for the terminal colors to work.
     requirements.append('colorama')
 
+
+def get_readme():
+    with open('README.rst') as readme:
+        return readme.read()
+
 setup(
     name='EasyEuler',
     packages=['EasyEuler', 'EasyEuler.commands'],
-    version='0.9',
+    version='0.9.1',
     description='A command line tool for Project Euler',
+    long_description=get_readme(),
     author='Encrylize',
     author_email='encrylize@gmail.com',
     url='https://github.com/Encrylize/EasyEuler',
