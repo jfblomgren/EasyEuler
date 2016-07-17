7bdc81f8fc1fd069c81eea622ae4f9bc31dcb8d5
Encrylize <encrylize@gmail.com>
2016-07-17 21:08:30 +0200

    Remove long flag

--- EasyEuler/commands/list.py
+++ EasyEuler/commands/list.py
@@ -4,21 +4,12 @@ from EasyEuler import data
 
 
 @click.command()
-@click.option('--long', '-l', is_flag=True,
-              help='Include problem descriptions.')
 @click.option('--sort', '-s', type=click.Choice(['id', 'difficulty']),
               default='id', help='Sort the list by problem attribute.')
-def cli(long, sort):
+def cli(sort):
     """ Lists all available problems. """
 
     problem_list = []
     for problem in sorted(data.problems, key=lambda p: p[sort.lower()]):
-        problem_string = 'Problem {id}: {name}'.format(**problem)
-
-        if long:
-            problem_string += '\n=========================================\n\n'
-            problem_string += problem['description']
-            problem_string += '\n\n========================================='
-
-        problem_list.append(problem_string)
+        problem_list.append('Problem {id}: {name}'.format(**problem))
     click.echo_via_pager('\n'.join(problem_list))
