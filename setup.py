23f8046c95cd209afb19f94625badb2e521cf9f6
Encrylize <encrylize@gmail.com>
2016-07-17 13:22:15 +0200

    Add package data

--- setup.py
+++ setup.py
@@ -11,11 +11,18 @@ if 'win32' in sys.platform.lower():
 
 setup(
     name='EasyEuler',
-    version='0.1',
-    py_modules=['EasyEuler'],
+    packages=['EasyEuler', 'EasyEuler.commands'],
+    version='0.9',
+    description='A command line tool for Project Euler',
+    author='Encrylize',
+    author_email='encrylize@gmail.com',
+    url='https://github.com/Encrylize/EasyEuler',
     install_requires=requirements,
     entry_points='''
         [console_scripts]
         easyeuler=EasyEuler.cli:cli
-    '''
+    ''',
+    keywords=['EasyEuler', 'ProjectEuler', 'euler', 'Project-Euler'],
+    include_package_data=True,
+    zip_safe=False
 )
