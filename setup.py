cd852d74c1e65f55257392f0f518e36fc821676e
Encrylize <encrylize@gmail.com>
2016-07-17 15:42:59 +0200

    Add license and classifiers to setup

--- setup.py
+++ setup.py
@@ -16,19 +16,31 @@ def get_readme():
 
 setup(
     name='EasyEuler',
-    packages=['EasyEuler', 'EasyEuler.commands'],
     version='0.9.1',
     description='A command line tool for Project Euler',
     long_description=get_readme(),
+    license='MIT',
     author='Encrylize',
     author_email='encrylize@gmail.com',
     url='https://github.com/Encrylize/EasyEuler',
+    keywords=['EasyEuler', 'ProjectEuler', 'euler', 'Project-Euler'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Utilities',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3.5',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: Unix',
+        'Natural Language :: English'
+    ],
+    packages=['EasyEuler', 'EasyEuler.commands'],
     install_requires=requirements,
     entry_points='''
         [console_scripts]
         easyeuler=EasyEuler.cli:cli
     ''',
-    keywords=['EasyEuler', 'ProjectEuler', 'euler', 'Project-Euler'],
     include_package_data=True,
     zip_safe=False
 )
